4d1665abcea251ea2fdbc0c1d2371e312f2f82e2
WonderofU8 <135637640+WonderofU8@users.noreply.github.com>
2023-12-26 19:43:55 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+﻿//toggle class active
+const navbarNav = document.querySelector
+    ('.navbar-nav')
+
+//ketika hamburger menu di click
+document.querySelector('#hamburger-menu').onclick = () => {
+    navbarNav.classList.toggle('active')
+
+    const hamburger = document.querySelector('#hamburger-menu');
+
+    document.addEventListener('click', (e) => {
+        if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
+            navbarNav.classList.remove('active');
+        }
+    });
+
+
+};
+
+
